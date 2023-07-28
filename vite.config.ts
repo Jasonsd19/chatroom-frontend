cac04ec805d14710ab6c8b26b0655574b07f3a69
Jason Deol <jasonsinghdeol@gmail.com>
2023-07-27 18:35:21 -0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: '../dist'
-  },
   plugins: [svelte()]
 })
